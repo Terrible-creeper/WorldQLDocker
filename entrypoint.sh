fbe950191c6815274b02007bedd912afa97d6d28
Terrible-creeper <32015407+Terrible-creeper@users.noreply.github.com>
2021-11-11 19:19:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-cd /home/container
+
+TZ=${TZ:-UTC}
+export TZ
+
+INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP
+
+cd /home/container || exit 1
+
 WQL_POSTGRES_CONNECTION_STRING = postgresql://${SQLUSER}:${SQLPASSWORD}@${SQLADDRESS}?port=${SQLPORT}&dbname=${SQLDATABASE}
+
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$STARTUP"
 
 /etc/init.d/postgresql start
 
-${STARTUP}
+exec env ${STARTUP}
