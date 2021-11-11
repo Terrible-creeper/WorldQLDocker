0c6795ae955d617b498fa05372957b75412e4860
Terrible-creeper <32015407+Terrible-creeper@users.noreply.github.com>
2021-11-11 19:28:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ export INTERNAL_IP
 
 cd /home/container || exit 1
 
+WQL_LEAF_SQUARE_SIZE=16
+WQL_TREE_DEGREE=512
+WQL_NUM_LEVELS=2
+WQL_ROOTS_PER_TABLE=8
 WQL_POSTGRES_CONNECTION_STRING=postgresql://${SQLUSER}:${SQLPASSWORD}@${SQLADDRESS}?port=${SQLPORT}&dbname=${SQLDATABASE}
 
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$STARTUP"
