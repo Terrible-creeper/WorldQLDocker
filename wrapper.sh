8b690d4bc58fa99ebf2990d0af143cecf498d2ba
Terrible-creeper <32015407+Terrible-creeper@users.noreply.github.com>
2021-11-11 17:53:56 +0800

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /home/container
+WQL_POSTGRES_CONNECTION_STRING = postgresql://${SQLUSER}:${SQLPASSWORD}@${SQLADDRESS}?port=${SQLPORT}&dbname=${SQLDATABASE}
 /etc/init.d/postgresql start
-./WorldQLServer -server
