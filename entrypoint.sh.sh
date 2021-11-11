63727486a4c4150ab8256bb0510420072ed9df1b
Terrible-creeper <32015407+Terrible-creeper@users.noreply.github.com>
2021-11-11 18:17:06 +0800

    Update and rename wrapper.sh to entrypoint.sh.sh

--- entrypoint.sh.sh
+++ entrypoint.sh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/container
 WQL_POSTGRES_CONNECTION_STRING = postgresql://${SQLUSER}:${SQLPASSWORD}@${SQLADDRESS}?port=${SQLPORT}&dbname=${SQLDATABASE}
+
 /etc/init.d/postgresql start
+
 ${STARTUP}
